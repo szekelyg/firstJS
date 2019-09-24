b3301311fbaa5f5594ff6075acc11a132e9449e1
szekelyg <36407132+szekelyg@users.noreply.github.com>
2019-09-24 13:59:18 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,16 @@
+var randomNumber1 = Math.floor(Math.random() * 6 + 1);
+var randomNumber2 = Math.floor(Math.random() * 6 + 1);
+
+var imageSeurce1 = "images/dice" + randomNumber1 + ".png";
+var imageSeurce2 = "images/dice" + randomNumber2 + ".png";
+
+document.querySelector('.img1').setAttribute("src", imageSeurce1);
+document.querySelector('.img2').setAttribute("src", imageSeurce2);
+
+if(randomNumber1 > randomNumber2){
+  document.querySelector("h1").innerText = "🚩 Player 1 wins!";
+}else if(randomNumber1 < randomNumber2) {
+  document.querySelector("h1").innerText = "Player 2 wins! 🚩";
+}else{
+  document.querySelector("h1").innerText = "Draw!";
+}
